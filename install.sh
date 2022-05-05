c17b47f1dcc799223c19e2cd36e4bae7752b8014
Marc Chevalier <marc.chevalier@snyk.io>
2022-05-05 17:42:58 +0200

    + opam file

--- install.sh
+++ install.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-make -j16 -C "src/util"
-make -C "src/mldiff"
-make -j16 -C "src/otreediff"
-make -j64 -C "src/ast/analyzing/common"
-make -j64 -C "src/ast/analyzing/engine"
-make -j16 -C "src/ast/analyzing/langs/astml"
-make -j64 -C "src/ast/analyzing/langs/common"
-make -j64 -C "src/ast/analyzing/langs/cpp/parsing/src"
-make -j64 -C "src/ast/analyzing/langs/cpp"
-make -j64 -C "src/ast/analyzing"
+mkdir -p $1
+mkdir -p $2/cca
+mkdir -p $3
+cp src/ast/analyzing/etc/* $1
+cp src/ast/analyzing/bin/* $2/cca
+cp src/ast/analyzing/modules/Mcpp*.cmxs $3
